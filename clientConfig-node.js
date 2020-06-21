b7f62d23148c6eac3d5404ef5c5f8990d8ad2af7
Juliane <julianewonka@Julianes-MBP.fritz.box>
2020-06-21 10:04:50 +0200

    Corrected typos. Added vercel.

---
+++ clientConfig-node.js
@@ -0,0 +1,12 @@
+const clientConfig = {
+  siteUrl: 'https://serious-salmon.com',
+  graphqlUrl: 'https://serious-salmon.com/graphql',
+  userIdName: 'userId',
+  authTokenName: 'authToken',
+  refreshTokenName: 'refreshToken',
+  tokenExpiry: 7,
+  consentCookieName: 'consent',
+  demoMsgCookieName: 'showDemoMsg',
+};
+
+module.exports = clientConfig;
