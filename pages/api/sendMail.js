866cccdcb0729aaf8a67ed5feb7c1d292e0d566e
Juliane <julianewonka@Julianes-MBP.fritz.box>
2020-06-28 10:23:55 +0200

    Renamed SENDER_MAIL_ADDRESS to SENDER_MAIL

--- pages/api/sendMail.js
+++ pages/api/sendMail.js
@@ -6,7 +6,7 @@ export default async (req, res) => {
   const { name, email, message } = req.body;
   const content = {
     to: process.env.RECIPIENT_MAIL,
-    from: process.env.SENDER_MAIL_ADDRESS,
+    from: process.env.SENDER_MAIL,
     subject: `New Message From - ${name} (${email})`,
     text: `Name: ${name}, Email: ${email}\nMessage: ${message}`,
     html: `<p>Name: ${name}</p><p>Email: ${email}</p><p>Message: ${message}</p>`,
